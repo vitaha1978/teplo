d75451a6c54002afec6fada4e6d50964813b7571
vitaha1978 <info@teplomarket.com.ua>
2014-11-28 10:56:18 +0000

    Change home page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :articles
   
   get ':url' => 'articles#show'
-  root 'static_pages#contacts'
+  root 'articles#index'
   match '/contacts', to: 'static_pages#contacts', via: 'get'
   
 
