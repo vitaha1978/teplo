093c1c73aef9fc29d68e2f23812f744cd6434041
vitaha1978 <info@teplomarket.com.ua>
2015-01-21 09:13:21 +0000

    Add some validations for Products

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,12 @@
 class Company < ActiveRecord::Base
   extend FriendlyId
   
+  validates :name, :img, :url, :describe, presence: true
+  validates :img, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
+  }
+  
   friendly_id :url
   
   has_many :articles
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :img, :title, :description, :category_id, :company_id, presence: true  
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :img, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
+  }
 end
