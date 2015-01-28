73180d6102a83b02de9c79ce54db2ecf6fc50fe8
vitaha1978 <info@teplomarket.com.ua>
2015-01-28 12:15:01 +0000

    Add new article and img

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   
-  resources :products
+  get 'store/index'
 
   root 'static_pages#index'
+  
   match '/contacts', to: 'static_pages#contacts', via: 'get'
   match '/', to: 'static_pages#index', via: 'get'
   
+  resources :products
   resources :companies
   resources :users
   resources :categories
