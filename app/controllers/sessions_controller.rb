e2639b9af2fcaf33299401811c1574ee0eeed967
vitaha1978 <info@teplomarket.com.ua>
2015-02-11 07:51:52 +0000

    Add User controller with pass field

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:name])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url
+    else
+      redirect_to login_url, alert: "Невірна комбінація імені та паролю"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to store_url, notice: "Сеанс роботи завершено"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150210141247) do
+ActiveRecord::Schema.define(version: 20150210143016) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -83,6 +83,13 @@ ActiveRecord::Schema.define(version: 20150210141247) do
 
   create_table "themes", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
