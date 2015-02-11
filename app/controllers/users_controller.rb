1cada63514c7b136b0ea93d935ce18b63913e95e
vitaha1978 <info@teplomarket.com.ua>
2015-02-11 09:22:10 +0000

    Add Admin filter for controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :authorize, only: [:index, :show, :create, :new]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
